80df2f9651ad678ad0623a1d61b33f86d511dcd8
BT_1N3_23@bktech.local <BT_1N3_23@bktech.local>
2018-10-23 11:37:54 -0400

    Initial commit

---
+++ src/com/company/HorseBarn.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class HorseBarn {
+    private Horse[] spaces;
+
+}
---
+++ src/com/company/horseRunner.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class horseRunner {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
